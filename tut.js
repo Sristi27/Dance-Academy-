25b6f964ab90d4e8a45814b3cf6ba93641e55937
Sristi27 <sristi2705@gmail.com>
2020-05-31 17:55:03 +0530

    commit

---
+++ tut.js
@@ -0,0 +1,9 @@
+//inserting data in database
+use sristidb
+db.items.insertOne({name:"phone",price:20000,sold:98})
+//for one
+db.items.insertMany
+//for many
+db.items.find()
+//see all the data
+//data gets stored even after weclose the mongo d process and rerun it again after some time
