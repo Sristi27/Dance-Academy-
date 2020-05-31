5e714baa5cf31412dc55347e3891886d81a4a033
Sristi27 <sristi2705@gmail.com>
2020-05-31 22:55:24 +0530

    commit

--- tut.js
+++ tut.js
@@ -7,3 +7,22 @@ db.items.insertMany
 db.items.find()
 //see all the data
 //data gets stored even after weclose the mongo d process and rerun it again after some time
+
+//search items
+db.items.find({})
+              //filter object 
+db.items.find({price:{$gte:58}})   //greater than or equl to 58
+//gt=greater than
+//lt=less than
+
+//and operator
+({price:{$gte: },sold:{$gte: }})
+
+//or operator
+({ $or:[{price:{$gte: 86}},{sold:{$lt:80}}]})
+
+//to see only price of the objects
+db.items.find({price:{$gte:50}}, {price:1}) 
+
+{price:1,sold:1}
+//shows price and sold
